7565b1c690b57c56bb32c9c0652a17598874a37c
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2024-11-29 02:29:06 -0300

    criando os bottoes de  delet e edit

--- src/Views/UserList.js
+++ src/Views/UserList.js
@@ -1,14 +1,65 @@
 import React from "react";
-import { FlatList, Text, View } from "react-native";
+import { Alert, FlatList, Text, View } from "react-native";
 import users from "../data/users";
+import { Avatar, Button, ListItem, } from "react-native-elements";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default props => {
+
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress() {
+                    console.warn('delete' + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="create-outline" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={() => confirmUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="trash-outline" size={25} color="red" />}
+                />
+            </>
+        )
+    }
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem bottomDivider key={user.id} onPress={() => props.navigation.navigate('UserForm', user)}>
+
+                <Avatar source={{ uri: user.avatarUrl }} />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+
+                </ListItem.Content>
+                <ListItem.Content right>
+                    {getActions(user)}
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
 
     return (
         <View>
             <FlatList
                 keyExtractor={users => users.id.toString()}
                 data={users}
+                renderItem={getUserItem}
             />
         </View>
     )
--- src/data/users.js
+++ src/data/users.js
@@ -3,28 +3,24 @@ export default [
         id: 1,
         name: 'Rosana Maria',
         email: 'rosana@email.com',
-        avatarUrl:
-            'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_1280.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_1280.png'
     },
     {
         id: 2,
         name: 'Joao Carlos',
         email: 'joao@email.com',
-        avatarUrl:
-            'https://cdn.pixabay.com/photo/2013/07/12/19/15/gangster-154425_1280.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/12/19/15/gangster-154425_1280.png'
     },
     {
         id: 3,
         name: 'Cristina silva',
         email: 'cris@email.com',
-        avatarUrl:
-            'https://cdn.pixabay.com/photo/2022/11/21/15/48/girl-7607431_1280.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2022/11/21/15/48/girl-7607431_1280.png'
     },
     {
         id: 4,
         name: 'michael santos',
         email: 'santos@email.com',
-        avatarUrl:
-            'https://cdn.pixabay.com/photo/2017/01/31/16/38/criminal-2025361_1280.png'
+        avatarUrl: 'https://cdn.pixabay.com/photo/2017/01/31/16/38/criminal-2025361_1280.png'
     }
 ]
