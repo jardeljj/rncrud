09f834d81b37fd6505e22583d571c99270a52b84
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2024-12-03 17:08:58 -0300

    criando o botão salvar e os campos de input

--- src/Views/UserForm.js
+++ src/Views/UserForm.js
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
-import { Text } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default ({ route, navigation }) => {
     const [user, setUSer] = useState(route.params ? route.params : {})
     return (
-        <Text>{user.id}</Text>
+        <View style={style.form}>
+            <Text>Name:</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={name => setUSer({ ...user, name })}
+                placeholder="Informe o Nome"
+                value={user.name}
+            />
+
+            <Text>Email:</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={email => setUSer({ ...user, email })}
+                placeholder="Informe o Email"
+                value={user.email}
+            />
+            <Text>URL do Avatar:</Text>
+            <TextInput
+                style={style.input}
+                onChangeText={avatarUrl => setUSer({ ...user, avatarUrl })}
+                placeholder="Informe a URL do Avatar"
+                value={user.avatarUrl}
+            />
+            <Button
+                title="Salvar"
+                onPress={() => {
+                    navigation.goBack()
+                }}
+            />
+        </View >
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 15,
+    }
+})
